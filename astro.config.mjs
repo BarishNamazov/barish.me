dc030b6c3577a9360037dfdfeaaccc933224bdfb
Abutalib (Barish) Namazov <barish2003@gmail.com>
2025-05-24 22:35:43 -0500

    Upgrade and add lessons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
-import remarkToc from "remark-toc";
+import remarkTocDynamic from "./src/plugins/remark-toc-dynamic";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://barish.me",
-  integrations: [mdx(), sitemap()],
-  markdown: {
-    remarkPlugins: [remarkToc]
-  }
+    site: "https://barish.me",
+    integrations: [mdx(), sitemap()],
+    markdown: {
+        remarkPlugins: [remarkTocDynamic]
+    }
 });
---
+++ src/plugins/remark-toc-dynamic.ts
@@ -0,0 +1,14 @@
+import remarkToc from "remark-toc";
+
+export default function remarkTocDynamic(options = {}) {
+    return function(tree, file) {
+        const { data } = file;
+        const tocHeading = data?.astro?.frontmatter?.tocHeading;
+
+        return remarkToc({
+            heading: tocHeading || options.heading || "Table of Contents",
+            ...options
+        })(tree, file);
+    };
+}
+
