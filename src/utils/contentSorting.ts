15de0721bd332430887af33b9cdb8a2605cdf83c
Abutalib (Barish) Namazov <barish2003@gmail.com>
2025-08-22 14:23:08 +0400

    Refactor sorting

---
+++ src/utils/contentSorting.ts
@@ -0,0 +1,13 @@
+export function sortContentByDate<T extends { data: { date: string | Date } }>(content: T[]): T[] {
+  return content.sort((a, b) => {
+    const aDate =
+      typeof a.data.date === 'string'
+        ? new Date(a.data.date + '-01')
+        : a.data.date;
+    const bDate =
+      typeof b.data.date === 'string'
+        ? new Date(b.data.date + '-01')
+        : b.data.date;
+    return bDate.valueOf() - aDate.valueOf();
+  });
+}
